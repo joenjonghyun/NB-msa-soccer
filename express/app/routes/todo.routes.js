64a79a11113861208c7f2be5bc828d26c15a7710
joenjonghyun <jjh980814@naver.com>
2022-04-08 18:45:09 +0900

    22.04.08 18:45

---
+++ express/app/routes/todo.routes.js
@@ -0,0 +1,12 @@
+module.exports = function(app){
+    app.use((req, res, next)=>{
+        res.header(
+
+        )
+    });
+    //next
+    app.get("",()=>{})
+    
+    app.update("",()=>{})
+    app.delete("",()=>{})
+}
